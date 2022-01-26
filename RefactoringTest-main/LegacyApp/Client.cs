a51f5b7ddb76e26bd332a75073e265cd480aacf8
algavi555 <algavi555@mail.ru>
2022-01-26 15:29:59 +0300

    Add files via upload

---
+++ RefactoringTest-main/LegacyApp/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace LegacyApp
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Rank { get; set; }
+        public ClientStatus ClientStatus { get; set; }
+    }
+}
